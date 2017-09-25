fa636886785f5e11039acb20e157e4c875850d66
Samuel <samueloluwatommzy@gmail.com>
2017-09-25 23:24:14 +0100

    implementing the wiki opensearch api

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var request  =  require("request");
 var yargs = require("yargs").argv;
- var url = "google.com";
- request(url,function(err,response,body){
+var argv  =  require("argv")
+//var searchword = argv.q|| " "
+var searchword = 'hello'
+//implementing wikipedia openSearch api
+var url ='https://en.wikipedia.org/w/api.php?action=opensearch&search="+ ${Michael Jackson}+ " &format=json';
+request(url,function(err,response,body){
      if (err){
-         console.log(err)
+         console.log(err);
      }
-     else {
-         console.log("body: " , body )
-     }
+      else {
+          var resp = JSON.parse(body);
+          for (var i =0;i < resp[1].length;i++){
+          var data = resp[2][i]
+          }
+         console.log("resp: " , data )
+      } 
  })
+ 
+  
+ 
